741f28b2fd69e2fbc0307a69299edaed601ba989
Ibnu <alyazzar14@gmail.com>
2023-08-27 20:41:34 +0700

    [IMPROVE] profile info

--- src/app/shared/components/profile-info/profile-info.component.ts
+++ src/app/shared/components/profile-info/profile-info.component.ts
@@ -11,6 +11,10 @@ export class ProfileInfoComponent {
   position: string = 'Fullstack Developer'
   image: string = './assets/images/profile/user-1.png'
 
+  urlGithub : string = 'https://github.com/'
+  urlLinkedin : string = 'https://www.linkedin.com/'
+  urlInstagram : string = 'https://www.instagram.com/'
+
   ngOnInit() {
     this.onImage(this.inputName)
   }
@@ -19,15 +23,27 @@ export class ProfileInfoComponent {
     if (name === 'dhimas') {
       this.name = 'Dhimas Pamungkas Setiabudi'
       this.image = `./assets/images/profile/user-dhimas.png`
+      this.urlGithub = 'https://github.com/'
+      this.urlLinkedin = 'https://www.linkedin.com/'
+      this.urlInstagram = 'https://www.instagram.com/'
     } else if (name === 'fredy') {
       this.name = 'Fredy Mordechai Marpaung'
       this.image = `./assets/images/profile/user-fredy.png`
+      this.urlGithub = 'https://github.com/'
+      this.urlLinkedin = 'https://www.linkedin.com/'
+      this.urlInstagram = 'https://www.instagram.com/'
     } else if (name === 'ibnu') {
       this.name = 'Muhammad Ibnu Al Yazzar'
       this.image = `./assets/images/profile/user-ibnu.jpg`
+      this.urlGithub = 'https://github.com/ibnu001'
+      this.urlLinkedin = 'https://www.linkedin.com/in/muhammadibnualyazzar/'
+      this.urlInstagram = 'https://www.instagram.com/ibnu.al.yazzar/'
     } else if (name === 'hafidz') {
       this.name = 'Vhierdy Hafidz'
       this.image = `./assets/images/profile/user-hafidz.png`
+      this.urlGithub = 'https://github.com/vhihafiz'
+      this.urlLinkedin = 'https://www.linkedin.com/in/vhierdy-hafidz-b12982232/'
+      this.urlInstagram = 'https://www.instagram.com/vhrdyhfdz/'
     }
   }
 }
